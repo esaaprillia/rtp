510c0f59e366ea6534b99baa488da7f9642b6840
esaaprillia <esaapriliasalsabila@gmail.com>
2026-05-31 07:45:11 +0800

    Update rfc3711.rs

--- src/rfc3711.rs
+++ src/rfc3711.rs
@@ -472,7 +472,7 @@ where
     ) {
         let iv = BigUint::from_bytes_be(&context.session_salt_key) << 16;
         let iv = iv ^ (BigUint::from(ssrc) << 64);
-        let iv = iv ^ (index.into() << 16);
+        let iv = iv ^ (Into::<BigUint>::into(index) << 16);
         let iv = iv ^ (BigUint::from(1_u8) << (context.session_encr_key.len() * 8));
         let iv = &iv.to_bytes_be()[1..context.session_encr_key.len() + 1];
 
@@ -506,7 +506,7 @@ where
     ) {
         let iv = BigUint::from_bytes_be(&context.session_salt_key) << 16;
         let iv = iv ^ (BigUint::from(ssrc) << 64);
-        let iv = iv ^ (index.into() << 16);
+        let iv = iv ^ (Into::<BigUint>::into(index) << 16);
         let iv = iv ^ (BigUint::from(1_u8) << (context.session_encr_key.len() * 8));
         let iv = &iv.to_bytes_be()[1..context.session_encr_key.len() + 1];
 
